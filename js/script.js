5f401fb96a81575788d40b06661d55de5cdb7059
cynoidit <cynoidit@gmail.com>
2022-05-03 16:42:30 -0700

    updates

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//An RPG where the player creates a character, fights in random dungeons, and builds a castle to train. Maybe handles multiple characters.
+
+//Character generation
+
+class Character{
+    constructor(name, job, level, hp, xp){
+        this.name = name
+        this.job = job
+        this.level = level
+        this.hp = hp
+        this.xp = xp
+    }
+}
+
+//Dungeon creator - Create entire dungeon at once or as the character progresses through
+
+
+//dungeon size
+//room size
+//room exits 
+
+class Dungeon{
+    constructor (size, type){
+        this.size = size
+        this.type = type
+    }
+}
+
+
+function roomRND(){
+
+}
+
+
+
+//Battle
+
+
+
+
+
+
